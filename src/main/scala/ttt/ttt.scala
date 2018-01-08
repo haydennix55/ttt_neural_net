2486cec5e755335a35939216b1384a69052aa26d
hani2284 <hani2284@colorado.edu>
2018-01-07 18:35:19 -0700

    reorganize tests and add intro comment

--- src/main/scala/ttt/ttt.scala
+++ src/main/scala/ttt/ttt.scala
@@ -1,6 +1,19 @@
 package ttt
 
+
 object game extends gameFunctions with App {
+
+  //BITBOARD TIC-TAC-TOE
+  //---------------------------
+  // Tic Tac Toe is a simple game. In fact, we can store the current game state in
+  // just an integer. We do this by starting with a blank board, a 0 integer. In binary,
+  // we consider 18 bits (eg. 000000000000000000). Each pair of bits, from least significant
+  // to most significant, represents a spot on the board. The rightmost pair of bits
+  // represents the top left square, or position 0. The next pair represents the top middle
+  // square, or position 1, etc.
+
+  // An empty square is represented by a 00, an X by a 01 (a shifted 1),
+  // and a Y by a 11 (a shifted 3).
 
 
 }
@@ -8,9 +21,12 @@ object game extends gameFunctions with App {
 
 trait gameFunctions {
 
+
+
   def newBoard(): Int = {
     0
   }
+
 
   def placePiece(player: Int, position: Int, board: Int): Int = {
 
@@ -19,12 +35,14 @@ trait gameFunctions {
 
   }
 
+
   def openSpot(spot: Int, board: Int): Boolean = {
 
     val mask = 3 << (2 * spot)
     if ((mask & board) == 0) true else false
 
   }
+
 
   def getWinner(board: Int): (Boolean,Option[String]) = {
 
@@ -47,8 +65,6 @@ trait gameFunctions {
     else if ((board & 0x3330) == 0x1110) (true, Some("X"))
 
     else (false,None)
-
-
 
   }
 
--- src/test/scala/ttt/tttSpec.scala
+++ src/test/scala/ttt/tttSpec.scala
@@ -4,10 +4,17 @@ import org.scalatest._
 
 class tttSpec extends FlatSpec {
 
+  //TEST newBoard
+  //---------------------------------
+
   "newBoard" should "return a 0" in {
     val board = game.newBoard()
     assert(board == 0)
   }
+
+
+  //TEST placePiece
+  //----------------------------------
 
   "placePiece" should "return 1 when given X and position 0" in {
     val X = 1
@@ -42,6 +49,9 @@ class tttSpec extends FlatSpec {
     assert(0x310 == finalBoard)
   }
 
+  //TEST openSpot
+  //----------------------------------
+
   "openSpot" should "return true when spot is open" in {
     val O = 3
     val nextBoard = game.placePiece(O,4,game.newBoard())
@@ -65,6 +75,9 @@ class tttSpec extends FlatSpec {
 
   }
 
+  //TEST getWinner
+  //----------------------------------
+
   "getWinner" should "return (false,None) when there is no winner in random board" in {
     val X = 1
     val O = 3
@@ -77,143 +90,122 @@ class tttSpec extends FlatSpec {
     board = game.placePiece(O,6,board)
 
     val result = game.getWinner(board)
-
     assert(!result._1)
 
   }
+
   it should "return (false,None) when there is an empty board" in {
     val board = game.newBoard()
     val result = game.getWinner(board)
-
     assert(!result._1)
 
   }
+
   it should "return (false,None) in this sneaky case" in {
     val board = 0x10F31 //010000111100110001
     val result = game.getWinner(board)
-
     assert(!result._1)
-
   }
+
   it should "return (false,None) when there is a cats game" in {
     val board = 0x375FD //110111010111111101
     val result = game.getWinner(board)
-
     assert(!result._1)
   }
+
   it should "return (true, X) when there is a top X win example" in {
-    val board = 0x3315
+    val board = 0x3315 //000011001100010101
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("X"))
-
-    //000011001100010101
   }
   it should "return (true,X) when there is a middle horiz X win example" in {
-    val board = 0x3543
+    val board = 0x3543 //000011010101000011
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("X"))
-    //000011010101000011
   }
+
   it should "return (true,X) when there is a bottom X win example" in {
-    val board = 0x15330
+    val board = 0x15330 //010101001100110000
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("X"))
-    //010101001100110000
   }
+
   it should "return (true,X) when there is a left X win example" in {
-    val board = 0x1F41
+    val board = 0x1F41 //000001111101000001
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("X"))
-    //000001111101000001
   }
+
   it should "return (true,X) when there is a middle vert X win example" in {
-    val board = 0x7107
+    val board = 0x7107 //000111000100000111
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("X"))
-    //000111000100000111
   }
+
   it should "return (true,X) when there is a right win example" in {
-    val board = 0x1071C
+    val board = 0x1071C //010000011100011100
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("X"))
-    //010000011100011100
   }
+
   it should "return (true,X) when there is a left to right diag X win example" in {
-    val board = 0x13131
+    val board = 0x13131 //010011000100110001
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("X"))
-    //010011000100110001
   }
+
   it should "return (true,X) when there is a right to left X win" in {
-    val board = 0x11D3
+    val board = 0x11D3 //000001000111010011
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("X"))
-    //000001000111010011
   }
+
   it should "return (true,O) when there is a top O win" in {
-    val board = 0x1017F
+    val board = 0x1017F //010000000101111111
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("O"))
-
-    //010000000101111111
   }
+
   it should "return (true,O) when there is a middle horiz O win" in {
-    val board = 0x1FC5
+    val board = 0x1FC5 //000001111111000101
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("O"))
-    //000001111111000101
   }
+
   it should "return (true,O) when there is a bottom O win" in {
-    val board = 0x3F114
+    val board = 0x3F114 //111111000100010100
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("O"))
-    //111111000100010100
   }
+
   it should "return (true,O) when there is a left O win" in {
-    val board = 0x34D7
+    val board = 0x34D7 //000011010011010111
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("O"))
-    //000011010011010111
   }
+
   it should "return (true,O) when there is a middle vert O win" in {
-    val board = 0x1D34C
+    val board = 0x1D34C //011101001101001100
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("O"))
-    //011101001101001100
   }
+
   it should "return (true,O) when there is a right O win" in {
-    val board = 0x30D35
+    val board = 0x30D35 //110000110100110101
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("O"))
-    //110000110100110101
   }
+
   it should "return (true,O) when there is a left to right diag O win" in {
-    val board = 0x31353
+    val board = 0x31353 //110001001101010011
     val result = game.getWinner(board)
-
     assert(result._1 && result._2 == Some("O"))
-    //110001001101010011
   }
-  it should "return (true,O) when there is a right to left diag O win" in {
-    val board = 0x3375
-    val result = game.getWinner(board)
 
+  it should "return (true,O) when there is a right to left diag O win" in {
+    val board = 0x3375 //000011001101110101
+    val result = game.getWinner(board)
     assert(result._1 && result._2 == Some("O"))
-    //000011001101110101
   }
 
 
